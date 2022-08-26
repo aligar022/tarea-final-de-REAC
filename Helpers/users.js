c9b7f0a79664c567566a69b32d23d47507e5f1c4
aligar022 <111609776+aligar022@users.noreply.github.com>
2022-08-26 18:24:17 -0500

    Add files via upload

---
+++ Helpers/users.js
@@ -0,0 +1,9 @@
+
+const users = [
+    { id: 21, name: 'Chepis'    },
+    { id: 22, name: 'Charito'    },
+    { id: 35, name: 'Mirna' },
+    { id: 48, name: 'Aracely' },
+    { id: 42, name: 'Rocío'  },
+    { id: 45, name: 'Laura' },
+  ]
---
+++ app.js
@@ -0,0 +1,4 @@
+const app = document.querySelector('#app');
+
+app.appendChild( Title('Clientas Mary Kay') );
+app.appendChild( CardList() );
